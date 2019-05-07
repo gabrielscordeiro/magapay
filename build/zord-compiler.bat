528770f6de5167c95ec209698c884fbb4814bccc
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2019-05-07 09:30:15 -0300

    Correção no .bat para compilar JS

--- build/zord-compiler.bat
+++ build/zord-compiler.bat
@@ -1,2 +1,2 @@
-java -jar compiler.jar --js "../web-magapay/js/jquery.mask.js" --js "../web-magapay/js/jquery-3.4.0.min.js" --js "../web-magapay/js/jquery-ui.min.js" --js "../web-magapay/js/owl.carousel.js" > "../web-magapay/js/magapay.js"
+java -jar compiler.jar --js "../web-magapay/js/jquery.mask.js" --js "../web-magapay/js/jquery-ui.min.js" --js "../web-magapay/js/owl.carousel.js" > "../web-magapay/js/magapay.js"
 pause
